a6fbe6f598ffb61b864cb1dd63160a93cfe58f7d
Michal Korman <michal.korman@icloud.com>
2023-10-08 21:47:30 +0200

    implement marking and umarking items on frontend

--- frontend/src/TodoItemsTable/TodoItemsTable.tsx
+++ frontend/src/TodoItemsTable/TodoItemsTable.tsx
@@ -1,8 +1,21 @@
 import './TodoItemsTable.css';
-import {useTodoItems} from '../hooks/todoItems';
+import {TodoItem, useTodoItems} from '../hooks/todoItems';
 
 const TodoItemsTable = () => {
-  const {items, itemsLoaded} = useTodoItems();
+  const {
+    items,
+    itemsLoaded,
+    markItemDone,
+    unmarkItemDone
+  } = useTodoItems();
+
+  const onMark = (item: TodoItem) => {
+    if (!item.done) {
+      markItemDone(item.id);
+    } else {
+      unmarkItemDone(item.id);
+    }
+  };
 
   if (!itemsLoaded) {
     return <></>;
@@ -13,6 +26,9 @@ const TodoItemsTable = () => {
       <tbody>
         {items.map((item, i) => (
           <tr key={i}>
+            <td className='items-table-td'>
+              <input type='checkbox' checked={item.done} onChange={() => onMark(item)}/>
+            </td>
             <td className='items-table-td'>{item.content}</td>
             <td className='items-table-td'>{item.createdAt.format('YYYY-MM-DD HH:mm')}</td>
           </tr>

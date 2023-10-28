774836a15f8ec2b34b4587eaa51210aa3a01f9b5
Michal Korman <michal.korman@icloud.com>
2023-10-28 13:59:22 +0200

    use in-memory h2 to store items

--- src/main/java/com/github/mkorman9/todo/TodoService.java
+++ src/main/java/com/github/mkorman9/todo/TodoService.java
@@ -2,39 +2,59 @@ package com.github.mkorman9.todo;
 
 import com.github.f4b6a3.uuid.UuidCreator;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.Instant;
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 @ApplicationScoped
 public class TodoService {
-    private final Map<UUID, TodoItem> items = new ConcurrentHashMap<>();
+    @Inject
+    DataSource dataSource;
 
     public List<TodoItem> getItems() {
-        return items.values()
-            .stream()
-            .sorted(Comparator.comparing(TodoItem::createdAt).reversed())
-            .toList();
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()
+        ) {
+            var resultSet = statement.executeQuery("SELECT * FROM todo_items ORDER BY created_at DESC");
+            var items = new ArrayList<TodoItem>();
+
+            while (resultSet.next()) {
+                var item = new TodoItem(
+                    (UUID) resultSet.getObject("id"),
+                    resultSet.getString("content"),
+                    resultSet.getBoolean("done"),
+                    resultSet.getTimestamp("created_at").toInstant()
+                );
+                items.add(item);
+            }
+
+            return items;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public UUID addItem(String content) {
         var id = UuidCreator.getTimeOrderedEpoch();
-        var now = Instant.now();
 
-        var existingItemId = items.putIfAbsent(id,
-            new TodoItem(
-                id,
-                content,
-                false,
-                now
-            )
-        );
-        if (existingItemId != null) {
-            throw new IllegalStateException("Item ID duplication");
+        try (var connection = dataSource.getConnection();
+             var statement = connection.prepareStatement(
+                 "INSERT INTO todo_items (id, content, done, created_at) VALUES (?, ?, ?, ?)"
+             )
+        ) {
+            statement.setObject(1, id);
+            statement.setString(2, content);
+            statement.setBoolean(3, false);
+            statement.setTimestamp(4, Timestamp.from(Instant.now()));
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
 
         return id;
@@ -49,16 +69,30 @@ public class TodoService {
     }
 
     public void deleteAll() {
-        items.clear();
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()
+        ) {
+            statement.execute("DELETE FROM todo_items");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void findAndMark(UUID id, boolean done) {
-        items.compute(id, (idFound, item) -> {
-            if (item == null) {
+        try (var connection = dataSource.getConnection();
+             var statement = connection.prepareStatement(
+                 "UPDATE todo_items SET done=? WHERE id=?"
+             )
+        ) {
+            statement.setBoolean(1, done);
+            statement.setObject(2, id);
+            var updated = statement.executeUpdate();
+
+            if (updated == 0) {
                 throw new TodoItemNotFoundException();
             }
-
-            return item.withDone(done);
-        });
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/resources/db/migration/V1.0.0__init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE todo_items (
+    id         uuid          primary key,
+    content    varchar(255),
+    done       boolean,
+    created_at timestamp
+);

68dde306f2717156b6e9886af651881354faa187
Michal Korman <michal.korman@icloud.com>
2023-10-30 22:02:25 +0100

    optimise imports

--- src/main/java/com/github/mkorman9/todo/TodoResource.java
+++ src/main/java/com/github/mkorman9/todo/TodoResource.java
@@ -10,9 +10,7 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 import org.jboss.resteasy.reactive.RestPath;
 import org.jboss.resteasy.reactive.RestResponse;
 

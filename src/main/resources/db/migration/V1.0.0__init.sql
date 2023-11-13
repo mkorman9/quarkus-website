8a4fe2920fd5975c13ec40845677781f8c0da256
Michal Korman <michal.korman@icloud.com>
2023-11-13 11:20:12 +0100

    add table keeping information about all action performed on item

--- src/main/resources/db/migration/V1.0.0__init.sql
+++ src/main/resources/db/migration/V1.0.0__init.sql
@@ -4,3 +4,10 @@ create table todo_items (
     done       boolean,
     created_at timestamp
 );
+
+create table todo_items_mark_actions (
+    id           uuid constraint todo_items_mark_actions_pkey primary key,
+    item_id      uuid constraint todo_items_mark_actions__todo_items_fkey references todo_items(id) on delete cascade,
+    target_value boolean,
+    created_at   timestamp
+);
